ae230d77db98e293b1b252bfebb64279dc5e4092
Nazir Rosas <nzgraphics.studio@gmail.com>
2024-09-28 23:00:43 +0000

    Set homepage for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nazir Rosas",
+  title: "Experienced Software Developer | Fullstack developer",
+  email: "rosasnazir@gmail.com",
+  gitHub: "barto12",
+  instagram: "nazir_12",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "nazirrosas",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Banco Simulado app for IOS devices",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "App desarrollada en Swift que muestra las funciones basicas de un app bancaria, como inicio de sesión, saldo, activar/desactivar tarjeta, y los movimientos bancarios realizados ",
+    url: "https://github.com/Barto12/BancoSimuladoApp",
   },
   {
-    title: "Web Development for Beginners",
+    title: "StockMarket app for IOS devices",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "App desarrollada en Swift que permite al usuario conocer el precio de una accion en el mercado de valores.",
+    url: "https://github.com/Barto12/StockMarket",
   },
   {
-    title: "My Resume Site",
+    title: "GradeMovieRecommender Python",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Programa desarrollado en Python utilizando machine learning con el uso de la libreria pandas",
+    url: "https://github.com/Barto12/GradeMovieRecommender",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "AutoPilot Python",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Programa desarrollado en Python que simula ser un software de conducción automatica de un automovil, el cual utiliza una red neuronal con la cual puede esquivar objetos en el trafico para evitar chocar con ellos, además de que el conductor en cualquier momento puede tomar el control del vehiculo.",
+    url: "https://github.com/Barto12/AutoPilot",
   },
 ];
 
